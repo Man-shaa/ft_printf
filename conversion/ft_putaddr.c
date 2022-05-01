83f66a8fe3c648ccfd1646b5e5403b1b0ed4bacf
Mansha <manuel.sharifi@gmail.com>
2022-05-01 15:36:47 +0200

    pointeur nul

--- conversion/ft_putaddr.c
+++ conversion/ft_putaddr.c
@@ -6,7 +6,9 @@ int ft_putaddr(unsigned long long int n)
 
     len = 0;
     if (!n)
-        return (len);
+    {
+        return(write(1, "(nil)", 5));
+    }
     len += write(1, "0x", 2);
     len += ft_puthex(n, "0123456789abcdef");
     return (len);
